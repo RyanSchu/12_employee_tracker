9e215a8a9b2c3b364a160bc75ad580d99cdf4ef4
Ryan Schubert <31641912+RyanSchu@users.noreply.github.com>
2022-11-03 23:07:38 -0500

    wired up user interface

--- server.js
+++ server.js
@@ -1,13 +1,60 @@
 const inquirer = require ('inquirer');
-const mysql = require('mysql2')
-const ctable = require('console.table')
+const handlers = require('./db/connection')
 
-const questions = [
+const mainMenuQuestions = [
     {
-        type: 'input',
-        message: 'please',
-        name: 'varname'
+        type: 'list',
+        choices: ["View all departments", 
+                  "View all roles", 
+                  "View all employees",
+                  "Add a department", 
+                  "Ad a role", 
+                  "Add an employee",
+                  "Update an employee role"],
+        message: "What would you like to do?",
+        name: 'choice'
     }
 ]
 
+
+
+const userMenu = async () => {
+    const userChoice = await inquirer.prompt([...mainMenuQuestions])
+    switch (userChoice.choice) {
+        case 'View all departments':
+            handlers.allEmployees()
+            userMenu()
+            break;
+        case 'View all roles':
+            handlers.allRoles()
+            userMenu()
+            break;
+        case 'View all employees':
+            handlers.allEmployees()
+            userMenu()
+            break;
+        // case value:
+        //     userMenu()
+        //     break;
+        // case value:
+        //     userMenu()
+        //     break;
+        // case value:
+        //     userMenu()
+        //     break;
+        // case value:
+        //     userMenu()
+        //     break;
+        default:
+            console.log("Choice not supported yet")
+            userMenu()
+            break;
+    }
+}
+
+userMenu()
+
+// handlers.allEmployees()
+// handlers.allRoles()
+// handlers.allDepartments()
 
